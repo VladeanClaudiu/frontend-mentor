cf6455083e0af524a468b9a2971565ff1bd330f1
VladeanClaudiu <claudiuver3.2@gmail.com>
2022-10-24 08:48:08 +0100

    added js api implementation

--- Advice generator app/index.js
+++ Advice generator app/index.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+const adviceBtn = document.getElementById("advice-button");
+const adviceNr = document.getElementById("advice-number");
+const adviceQuote = document.getElementById("advice-quote");
+
+async function getAdvice() {
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const data = await res.json();
+  console.log(await data);
+  adviceNr.innerText = `ADVICE #${data.slip.id}`;
+  adviceQuote.innerText = `"${data.slip.advice}"`;
+}
+
+adviceBtn.addEventListener("click", getAdvice);
