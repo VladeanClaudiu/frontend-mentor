8c294530c5c79903988503b88f041357fb46c08e
VladeanClaudiu <claudiuver3.2@gmail.com>
2022-10-28 16:45:58 +0100

    css changes

--- News Homepage/index.js
+++ News Homepage/index.js
@@ -1,6 +1,7 @@
 // Hambugeer button js class adder and remover
 const navButton = document.querySelector(".ham-menu");
 const nav = document.querySelector("nav");
+const overlay = document.querySelector(".container-overlay");
 
 let navOpen = false;
 
@@ -8,10 +9,12 @@ navButton.addEventListener("click", () => {
   if (!navOpen) {
     nav.classList.add("open-nav");
     navButton.classList.add("open");
+    overlay.classList.add("overlay-on");
     navOpen = true;
   } else {
     nav.classList.remove("open-nav");
     navButton.classList.remove("open");
+    overlay.classList.remove("overlay-on");
     navOpen = false;
   }
 });
